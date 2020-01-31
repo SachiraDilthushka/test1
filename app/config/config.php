59bcbf6c838f9450eaee1e9e5dac75c193119c0f
Induranga98 <indurangawarusamana@gmail.com>
2020-01-31 15:55:36 +0530

    htaccess modified

--- app/config/config.php
+++ app/config/config.php
@@ -8,6 +8,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/');
+  define('URLROOT', 'http://localhost/aaale');
   // Site Name
   define('SITENAME','AAAle');
